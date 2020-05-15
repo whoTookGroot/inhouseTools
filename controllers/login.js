0568b406f6126f8b0bdb36e83484f8ae3685038c
Erik-Grootendorst <1mdagroot@gmail.com>
2020-05-15 09:44:10 -0700

    Integrate Auth

--- controllers/login.js
+++ controllers/login.js
@@ -1,4 +1,13 @@
+const { google } = require('googleapis');
+const express = require('express');
+const base64url = require('base64url');
+const OAuth2Data = require('../data/google_key.json');
 
+const CLIENT_ID = OAuth2Data.web.client_id;
+const CLIENT_SECRET = OAuth2Data.web.client_secret;
+const REDIRECT_URL = OAuth2Data.web.redirect_uris;
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
 function getLogin(req,res,next){
     if(req.session.loggedIn)
@@ -6,14 +15,56 @@ function getLogin(req,res,next){
     else
         res.render('login',{
             pageTitle : 'Login',
-            path:''
+            path:'',
+            flashMsg: ''
         });
 }
+
+
 function postLogin(req,res,next){
-    req.session.loggedIn = true;
-    res.redirect('/');
+    const url = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'openid email profile',
+        hd:'gosite.com'
+    });
+    res.redirect(url);
 }
+
+
+function authCallback(req,res,next) {
+    const code = req.query.code
+    if (code) {
+        // Get an access token based on our OAuth code
+        oAuth2Client.getToken(code, function (err, tokens) {
+            if (err) {
+                console.log('Error authenticating')
+                console.log(err);
+            } else {
+                console.log('Google Successfully authenticated');
+                let header = tokens.id_token.split('.');
+                let hd = JSON.parse(base64url.decode(header[1])).hd;
+                if(hd =='gosite.com'){
+                    console.log('Client authed');
+                    oAuth2Client.setCredentials(tokens);
+                    req.session.loggedIn = true;
+                    res.redirect('/');
+                }
+                else{
+                    console.log('Client not authed');
+                    res.render('login',{
+                        pageTitle : 'Login',
+                        path:'',
+                        flashMsg: 'Invalid Credentials'
+                    });
+                }
+            }
+        });
+    }
+}
+
+
 module.exports ={
     getLogin: getLogin,
-    postLogin: postLogin
+    postLogin: postLogin,
+    authCallback: authCallback
 }
--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,8 @@ const router = express.Router();
 const loginCont = require('../controllers/login');
 
 router.get('/login', loginCont.getLogin);
-
 router.post('/login', loginCont.postLogin);
+router.get('/auth/google/callback', loginCont.authCallback);
+
 
 module.exports = router;
